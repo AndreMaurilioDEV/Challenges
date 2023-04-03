fb62f849053c8ce6ba1bfe0a7329896c32f910e9
André Maurilio <117940329+AndreMaurilioDEV@users.noreply.github.com>
2023-04-03 17:31:35 -0300

    last update

---
+++ menu.js
@@ -0,0 +1,8 @@
+var btnmobile = document.getElementById('btn-mobile')
+
+function toggleMenu(){
+    var nav = document.getElementById('nav')
+    nav.classList.toggle('active')
+}
+
+btnmobile.addEventListener('click',toggleMenu);
